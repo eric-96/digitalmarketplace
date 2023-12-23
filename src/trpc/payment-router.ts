9365e1704b2fb319b5e38bc7d085c335f56f8626
Eric Schwalb <ericpossollo@gmail.com>
2023-12-23 16:17:29 +0000

    fix: corrected typo causing stripe checkout to fail

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -1,106 +1,108 @@
-import { z } from "zod";
-import { privateProcedure, publicProcedure, router } from "./trpc";
-import { TRPCError } from "@trpc/server";
-import { getPayloadClient } from "../get-payload";
-import { stripe } from "../lib/stripe";
-import type Stripe from "stripe";
+import { z } from 'zod'
+import {
+  privateProcedure,
+  publicProcedure,
+  router,
+} from './trpc'
+import { TRPCError } from '@trpc/server'
+import { getPayloadClient } from '../get-payload'
+import { stripe } from '../lib/stripe'
+import type Stripe from 'stripe'
 
 export const paymentRouter = router({
-    createSession: privateProcedure.input(z.object({productIds: z.array(z.string())}))
-    .mutation(async ({input, ctx}) => {
-    const { user } = ctx
-    let { productIds } = input
+  createSession: privateProcedure
+    .input(z.object({ productIds: z.array(z.string()) }))
+    .mutation(async ({ ctx, input }) => {
+      const { user } = ctx
+      let { productIds } = input
 
-    if(productIds.length === 0) {
-        throw new TRPCError ({code: 'BAD_REQUEST'})
-    }
+      if (productIds.length === 0) {
+        throw new TRPCError({ code: 'BAD_REQUEST' })
+      }
 
-    const payload = await getPayloadClient()
+      const payload = await getPayloadClient()
 
-    const {docs: products} = await payload.find({
-    collection: 'products',
-    where: {
-        id: {
+      const { docs: products } = await payload.find({
+        collection: 'products',
+        where: {
+          id: {
             in: productIds,
+          },
         },
-        approvedForSale: {
-            equals: 'approved',
-        },
-    },
-    })
+      })
 
-    const filteredProducts = products.filter((prod) => Boolean(prod.priceId))
+      const filteredProducts = products.filter((prod) =>
+        Boolean(prod.priceId)
+      )
 
-    const order = await payload.create({
+      const order = await payload.create({
         collection: 'orders',
         data: {
-            _isPaid: false,
-            user: user.id,
-            products: filteredProducts.map((prod) => prod.id)
+          _isPaid: false,
+          products: filteredProducts.map((prod) => prod.id),
+          user: user.id,
         },
-    })
+      })
 
-    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = []
+      const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] =
+        []
 
-    filteredProducts.forEach((prod) => {
+      filteredProducts.forEach((product) => {
         line_items.push({
-            price: prod.priceId!,
-            quantity: 1,
-            adjustable_quantity: {
-                enabled: false,
-            },
+          price: product.priceId!,
+          quantity: 1,
         })
-    })
+      })
 
-    line_items.push({
+      line_items.push({
         price: 'price_1OQX61I3xnwtNZ2RjJ4ud4FD',
         quantity: 1,
         adjustable_quantity: {
-            enabled: false,
+          enabled: false,
         },
-    })
+      })
 
-    try {
-        const stripeSession = await stripe.checkout.sessions.create({
+      try {
+        const stripeSession =
+          await stripe.checkout.sessions.create({
             success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
             cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
             payment_method_types: ['card', 'paypal'],
             mode: 'payment',
             metadata: {
-                userId: user.id,
-                orderId: order.id,
+              userId: user.id,
+              orderId: order.id,
             },
             line_items,
-    })
+          })
 
-    return {url: stripeSession.url}
-} catch (err) {
-    console.log(err)
+        return { url: stripeSession.url }
+      } catch (err) {
+        return { url: null }
+      }
+    }),
+  pollOrderStatus: privateProcedure
+    .input(z.object({ orderId: z.string() }))
+    .query(async ({ input }) => {
+      const { orderId } = input
 
-    return {url: null}
-}
+      const payload = await getPayloadClient()
 
-}),
-
-pollOrderStatus: privateProcedure.input(z.object({orderId: z.string()})).query(async({input}) => {
-    const {orderId} = input
-
-    const payload = await getPayloadClient()
-
-    const {docs: orders} = await payload.find({
+      const { docs: orders } = await payload.find({
         collection: 'orders',
         where: {
-            id: {
-                equals: orderId,
-            },
+          id: {
+            equals: orderId,
+          },
         },
-    })
-    if(!orders.length) {
-        throw new TRPCError({code: 'NOT_FOUND'})
-    }
+      })
 
-    const [order] = orders
+      if (!orders.length) {
+        throw new TRPCError({ code: 'NOT_FOUND' })
+      }
 
-    return { isPaid: order._isPaid }
-})
+      const [order] = orders
+
+      return { isPaid: order._isPaid }
+    }),
 })
